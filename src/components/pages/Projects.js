f225f147450e78b30a413d6fa1bf907c5d030c64
Vinicius Farias <vfarias51@gmail.com>
2022-01-14 11:35:24 -0300

    Criado o componente projectCard para exibir os projetos cadastrados no 'banco'

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,18 @@
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min"
 
+import { useState, useEffect } from "react"
+
 import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+
+import styles from "./Projects.module.css"
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -10,10 +20,45 @@ function Projects() {
         message = location.state.message
     }
 
+    useEffect(() => {
+        // Para ver o loading
+        setTimeout(
+            () =>
+                fetch('http://localhost:5000/projects', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then((resp) => resp.json())
+                    .then((data) => {
+                        setProjects(data)
+                        setRemoveLoading(true)
+                    }),
+            100,
+        )
+    }, [])
+
     return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message type="success" msg={message}/> }
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container className="start">
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
+                    
+            </Container>
         </div>
     )
 }
